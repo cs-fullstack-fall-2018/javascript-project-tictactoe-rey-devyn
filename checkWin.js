2fa7be65589a4c2638f92e8d0683037ad9ff6ca6
student <you@example.com>
2018-08-22 13:21:31 -0500

    check in

---
+++ checkWin.js
@@ -0,0 +1,36 @@
+function arrayContainsArray (superset, subset) {
+    if (0 === subset.length) {
+        return false;
+    }
+    return subset.every(function (value) {
+        return (superset.indexOf(value) >= 0);
+    });
+
+}
+////////horizontal
+var winCombo1 =["0","1","2"];
+var winCombo2 = ["3","4","5"];
+var winCombo3 = ["6","7","8"];
+
+////////vertical
+var winCombo4 = ["0","3","6"];
+var winCombo5 = ["1","4","7"];
+var winCombo6 = ["2","5","8"];
+
+////////////Diagonal
+var winCombo7 = ["0","4","8"];
+var winCombo8 = ["6","4","2"];
+
+var Combos = [winCombo1, winCombo2, winCombo3, winCombo4,
+    winCombo5, winCombo6, winCombo7, winCombo8];
+
+function checkWin(playerAnswers){
+    for (var x = 0; x < Combos.length; x++){    //Iterate through Combos
+        if(arrayContainsArray(playerAnswers,Combos[x])){
+            alert("YOU WIN");
+        }else if(xArray.length + oArray.length === 9 ){
+            alert("CAT")
+        }
+
+    }
+}
--- t3.js
+++ t3.js
@@ -1,4 +1,45 @@
+var color = "red";
+var nowPlayer = "x";
+var xArray = [];
+var oArray = [];
+var classname = document.getElementsByClassName("tile");
+function switchPlayerTurn() {
+    if(nowPlayer === "x"){
+        nowPlayer = "o";
+        color = "blue"
+    }else{
+        nowPlayer = "x";
+        color = "red"
+    }
 
+
+}
+// Setup your Variables
+
+
+var changeTile = function() {
+    if(this.id !== "x" && this.id !== "o") {
+
+        this.style.backgroundColor = color;
+        if(nowPlayer === "x"){
+            xArray.push(this.id)
+        }else{
+            oArray.push(this.id)
+        }
+        this.id = nowPlayer;
+
+        switchPlayerTurn();
+        checkWin(xArray);
+        checkWin(oArray);
+
+    }
+
+
+};
+
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', changeTile, false);
+}
 // Setup your Variables
 
 // Handle Restart game button
